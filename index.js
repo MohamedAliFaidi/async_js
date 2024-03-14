fa775844763e2c48a6f229712245e983d31e6361
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-03-14 12:19:05 +0100

    sync js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Synchronous
+function bakeCake() {
+  let eggs = getEggs();
+  let flour = getFlour();
+  let sugar = getSugar();
+
+  // Now you can make the cake
+}
+
+// This function gets the eggs synchronously
+function getEggs() {
+  return "eggs";
+}
+// Similarly, getFlour and getSugar functions
+function getFlour() {
+  return "floor";
+}
+
+function getSugar() {
+  return "sugar";
+}
